130b3ca480c07bf9a0526a52cf57a88b7d621ca1
Gnaseel <hgnaseel@gmail.com>
2020-06-04 16:02:10 +0900

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,33 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Projects from "../views/Projects.vue";
-import About from "../views/About.vue";
-import Column from "../views/Column.vue";
-Vue.use(VueRouter);
+import Router from "vue-router";
+import Home from "../components/Home.vue";
+import Projects from "../components/Projects.vue";
+import About from "../components/About.vue";
+import Column from "../components/Column.vue";
+Vue.use(Router);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/Projects",
-    name: "Projects",
-    component: Projects
-  },
-  {
-    path: "/About",
-    name: "About",
-    component: About
-  },
-  {
-    path: "/Column",
-    name: "Column",
-    component: Column
-  },
-  
-];
-
-const router = new VueRouter({
-  routes
+export default new Router({
+  mode:'history',
+  routes:[
+      {
+        path: "/gnaseel/",
+        name: "Home",
+        component: Home
+      },
+      {
+        path: "/gnaseel/Projects",
+        name: "Projects",
+        component: Projects
+      },
+      {
+        path: "/gnaseel/About",
+        name: "About",
+        component: About
+      },
+      {
+        path: "/gnaseel/Column",
+        name: "Column",
+        component: Column
+      },
+  ]
 });
-
-export default router;
